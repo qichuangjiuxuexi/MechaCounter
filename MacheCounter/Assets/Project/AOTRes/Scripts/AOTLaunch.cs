e38ad484dac6e96bf7a221d7e156f9d24756f661
zhuang.he <zhuang.he@me2zen.com>
2023-11-11 23:25:58 +0800

    AOT场景预制

---
+++ MacheCounter/Assets/Project/AOTRes/Scripts/AOTLaunch.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class AOTLaunch : MonoBehaviour
+{
+    protected void Start()
+    {
+        /*var assembly = Assembly.Load("HotfixAsm");
+        var type = assembly.GetType("LaunchLoadingControl");
+        var method = type.GetMethod("Create", BindingFlags.Public | BindingFlags.Static);
+        method.Invoke(null, new object[] { });*/
+    }
+}
