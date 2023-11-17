5946a399a01b26231dde47855ebed977c0eb4823
qiuguoliang <guoliang.qiu@me2zen.com>
2023-11-17 19:07:19 +0800

    ui manager  部分方法添加

--- MacheCounter/Assets/Project/Scripts/GameUI/Framework/GameUIManager.cs
+++ MacheCounter/Assets/Project/Scripts/GameUI/Framework/GameUIManager.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class GameUIManager : MonoBehaviour
 {
+    public static GameUIManager Instance;
+
+    public void ShowView(int viewId)
+    {
+        UIConfig config = UIConfigManager.GetConfig(viewId);
+        //TODO: 1、获取到perfab 2、创建
+    }
     
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(Instance.gameObject);
+            Instance = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Instance = null;
+    }
 }
