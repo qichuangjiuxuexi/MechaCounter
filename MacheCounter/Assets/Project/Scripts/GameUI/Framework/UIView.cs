2e438f721e13eb3e8b3d9763c2e14eb69dca89ac
qiuguoliang <guoliang.qiu@me2zen.com>
2023-11-17 18:55:02 +0800

    ui view 部分方法添加

--- MacheCounter/Assets/Project/Scripts/GameUI/Framework/UIView.cs
+++ MacheCounter/Assets/Project/Scripts/GameUI/Framework/UIView.cs
@@ -5,8 +5,41 @@ using UnityEngine;
 public class UIView : MonoBehaviour
 {
     protected UIConfig mUiConfig;
+    protected RectTransform mViewRectTransform;
+    
+    public virtual void Awake()
+    {
+        Initialize();
+    }
 
 
+    //#region 数据相关
+    protected void Initialize()
+    {
+        mViewRectTransform = gameObject.GetComponent<RectTransform>();
+    }
+    
+    
+    //view 放在哪个层级
+    public int LayerType
+    {
+        get
+        {
+            if (mUiConfig != null)
+            {
+                return mUiConfig.layerType;
+            }
+
+            return UIConfigManager.None;
+        }
+    }
+
+    public void SetUIConfig(UIConfig uiConfig)
+    {
+        mUiConfig = uiConfig;
+    }
+    
+    
     public int GetViewId()
     {
         if (mUiConfig != null)
@@ -16,4 +49,32 @@ public class UIView : MonoBehaviour
 
         return 0;
     }
+    //#endregion
+    
+    
+    //#region ui相关
+    public void SetActive(bool active)
+    {
+        gameObject.SetActive(active);
+
+        if (active)
+        {
+            OnShow();
+        }
+        else
+        {
+            OnHide();
+        }
+    }
+    
+    public virtual void OnShow()
+    {
+
+    }
+
+    public virtual void OnHide()
+    {
+
+    }
+    //#endregion
 }
